41939ebafccc017fd580c2458872bb3dcfd7b25c
NathamonK <siriwan.p3132@gmail.com>
2024-08-21 14:55:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 df = sns.load_dataset('iris')
 df
 
@@ -17,8 +20,9 @@ knn.fit(x_train, y_train)
 
 st.sidebar.title('Classifier')
 classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'DT', 'NN', 'RF'))
+k = st.sidebar.slider('K', 1, 20, 3)
 if classifier =="KNN":
-  knn = KNeighborsClassifier(n_neighbors=3)
+  knn = KNeighborsClassifier(n_neighbors=k)
   knn.fit(x_train, y_train)
   y_pred = knn.predict(x_test)
   accuracy_score(y_test, y_pred)
@@ -27,3 +31,19 @@ if classifier =="SVM":
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
   accuracy_score(y_test, y_pred)
+if classifier =="DT":
+  dt = DecisionTreeClassifier()
+  dt.fit(x_train, y_train)
+  y_pred = dt.predict(x_test)
+  accuracy_score(y_test, y_pred)
+if classifier =="NN":
+  nn = MLPClassifier()
+  nn.fit(x_train, y_train)
+  y_pred = nn.predict(x_test)
+  accuracy_score(y_test, y_pred)
+if classifier =="RF":
+  rf = RandomForestClassifier()
+  rf.fit(x_train, y_train)
+  y_pred = rf.predict(x_test)
+  accuracy_score(y_test, y_pred)
+  
